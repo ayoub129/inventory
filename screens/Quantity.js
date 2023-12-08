632f74540276ae485446d455fdd15773b84530b0
Ayoub Berouijil <80260703+ayoub129@users.noreply.github.com>
2023-12-08 20:31:03 +0100

    Update Quantity.js

--- screens/Quantity.js
+++ screens/Quantity.js
@@ -1,13 +1,20 @@
-import { View, Text, Image, TextInput, StyleSheet } from "react-native";
 import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  StyleSheet,
+  PermissionsAndroid,
+} from "react-native";
 import DropDown from "../components/DropDown";
 import Button from "../components/Button";
-import RNFetchBlob from "react-native-fetch-blob";
+import * as FileSystem from "expo-file-system";
 
 const Quantity = () => {
   const [formData, setFormData] = useState({
     Team: "",
-    Part_Number: 25,
+    Part_Number: "",
     Quantity: "",
     Date: "",
     Time: "",
@@ -20,7 +27,7 @@ const Quantity = () => {
     }));
   };
 
-  const QuantityChange = (text) => {
+  const SerialChange = (text) => {
     setFormData((prevState) => ({
       ...prevState,
       Quantity: text,
@@ -34,76 +41,130 @@ const Quantity = () => {
     }));
   };
 
-  const HandlePress = () => {
-    setFormData((prevState) => ({
-      ...prevState,
-      Date: new Date.toLocaleDateString("en-US", {
-        month: "2-digit",
-        day: "2-digit",
-        year: "numeric",
-      }),
-      Time: new Date.toLocaleTimeString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-      }),
-    }));
+  const HandlePress = async () => {
+    try {
+      setFormData((prevState) => ({
+        ...prevState,
+        Date: new Date().toLocaleDateString("en-US", {
+          month: "2-digit",
+          day: "2-digit",
+          year: "numeric",
+        }),
+        Time: new Date().toLocaleTimeString("en-US", {
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+      }));
 
-    // convert to CSV by data
-    const convertToCSV = (data) => {
-      // Headers of the CSV File
-      const header = Object.keys(data[0]).join(",");
-      // Data Handling
-      const rows = data.map((item) => Object.values(item).join(","));
-      return `${header}\n${rows.join("\n")}`;
-    };
+      // convert to CSV by data
+      const convertToCSV = (data) => {
+        const dataArray = Array.isArray(data) ? data : [data];
+        const header = Object.keys(dataArray[0]).join(",");
+        const rows = dataArray.map((item) => Object.values(item).join(","));
+        return `${header}\n${rows.join("\n")}`;
+      };
 
-    async function exportToCSV(data) {
-      const csvData = convertToCSV(data);
+      const requestPermission = async () => {
+        try {
+          const granted = await PermissionsAndroid.requestMultiple(
+            [
+              PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+              PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+            ],
+            {
+              title: "You need to provide access to the file system",
+              message:
+                "Inventory App needs access to your storage " +
+                "so you can save and read CSV files.",
+              buttonNeutral: "Ask Me Later",
+              buttonNegative: "Cancel",
+              buttonPositive: "OK",
+            }
+          );
+          if (
+            granted["android.permission.WRITE_EXTERNAL_STORAGE"] ===
+              PermissionsAndroid.RESULTS.GRANTED &&
+            granted["android.permission.READ_EXTERNAL_STORAGE"] ===
+              PermissionsAndroid.RESULTS.GRANTED
+          ) {
+            // Use documentDirectory to get the root directory for files
+            const fileUri = `${FileSystem.documentDirectory}Inventory1.csv`;
 
-      // Create a file path
-      const filePath = `${RNFetchBlob.fs.dirs.DocumentDir}/inventory2.csv`;
+            // Read existing file content
+            let existingContent = "";
+            try {
+              existingContent = await FileSystem.readAsStringAsync(fileUri);
+            } catch (readError) {
+              console.warn("Error reading file:", readError);
+            }
 
-      const fileExists = await RNFS.exists(filePath);
+            // Check if the file is empty or contains headers
+            const isFileEmpty = existingContent.trim() === "";
+            const containsHeaders =
+              existingContent.startsWith(
+                "Team,Part_Number,Quantity,Date,Time"
+              ) ||
+              existingContent.includes("\nTeam,Part_Number,Quantity,Date,Time");
 
-      if (!fileExists) {
-        // Write the CSV data to a file
-        RNFetchBlob.fs
-          .writeFile(filePath, csvData, "utf8")
-          .then(() => {
-            // CSV file created successfully, you can do further actions like sharing or displaying a success message.
-            console.log("CSV file created successfully:", filePath);
-          })
-          .catch((error) => {
-            console.error("Error creating CSV file:", error);
-          });
-      } else {
-        // Append new data to the file
-        await RNFS.appendFile(filePath, csvData, "utf8");
-      }
+            // Append new data without headers if they are already present
+            const newDataWithoutHeaders = convertToCSV(formData)
+              .split("\n")
+              .slice(1)
+              .join("\n");
+
+            const updatedContent =
+              isFileEmpty || !containsHeaders
+                ? `${existingContent}${convertToCSV(formData)}`
+                : `${existingContent}\n${newDataWithoutHeaders}`;
+
+            try {
+              await FileSystem.writeAsStringAsync(fileUri, updatedContent);
+              console.log("File updated or created successfully.");
+            } catch (writeError) {
+              console.error("Error writing to file:", writeError);
+            }
+          }
+        } catch (err) {
+          console.warn(err);
+        }
+      };
+
+      requestPermission();
+    } catch (error) {
+      console.error("Error handling press:", error);
     }
-
-    exportToCSV(formData);
   };
 
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <Image
-        source={require("../assets/images/lear.png")}
+        source={require("../assets/images/Lear.png")}
         style={Styles.image}
       />
-      <View>
+      <View style={Styles.dropdown}>
         <Text>Team</Text>
         <DropDown getTeamData={getTeamData} />
       </View>
-      <View>
+      <View style={Styles.container}>
         <Text>port number</Text>
-        <TextInput onChangeText={PortChange} value={formData.Part_Number} />
+        <TextInput
+          onChangeText={PortChange}
+          value={formData.Part_Number.toString()}
+          style={Styles.input}
+        />
       </View>
-      <View>
-        <Text>Serial Number</Text>
-        <TextInput onChangeText={QuantityChange} value={formData.Quantity} />
+      <View style={Styles.container}>
+        <Text>Quantity</Text>
+        <TextInput
+          onChangeText={SerialChange}
+          value={formData.Quantity}
+          style={Styles.input}
+          keyboardType="numeric"
+        />
       </View>
-      <Button onPress={HandlePress}>Save</Button>
+      <View style={Styles.container}>
+        <Button onPress={HandlePress}>Save</Button>
+      </View>
     </View>
   );
 };
@@ -111,7 +172,23 @@ const Quantity = () => {
 export default Quantity;
 const Styles = StyleSheet.create({
   image: {
-    width: "100%",
-    height: "100%",
+    width: "80%",
+    height: "80%",
+    marginHorizontal: "10%",
+    marginTop: 32,
+    height: "25%",
+  },
+  dropdown: {
+    marginHorizontal: "10%",
+    marginBottom: 22,
+  },
+  input: {
+    padding: 8,
+    borderColor: "#000",
+    borderBottomWidth: 1,
+    marginBottom: 24,
+  },
+  container: {
+    marginHorizontal: "10%",
   },
 });
